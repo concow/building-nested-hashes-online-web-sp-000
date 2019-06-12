8bdb713f54dfea7be050a0d64cb59c48b4e38307
Connor Cowan <ccowan003@comcast.net>
2019-06-12 22:20:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,9 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague" age: "53"},      #here in the 3rd level. we used the other syntax for 'key: value'
-      :matriarch => {name: "Lady Montague" age: "54"
-        #your key/value pairs here
-        },
+      :matriarch => {name: "Lady Montague" age: "54"},
       :hero => {},
       :hero_friends => []
    },
