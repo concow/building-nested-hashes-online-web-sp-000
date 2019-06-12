8440ecbc54c9bc2054a3eaff132747fdaf8bddaa
Connor Cowan <ccowan003@comcast.net>
2019-06-12 22:07:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,6 +9,10 @@ def second_challenge
       },
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {},
+      :martriach => {},
+      :hero => {},
+      :hero_friends => []
       }}
 
 
